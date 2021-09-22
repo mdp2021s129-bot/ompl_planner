240f1dd65e07cc6b4eaca17921546eecdada5c28
Shenghao Yang <me@shenghaoyang.info>
2021-09-22 23:36:35 +0800

    main: setup gRPC server

--- ompl.cpp
+++ ompl.cpp
@@ -33,15 +33,20 @@
  *********************************************************************/
 
 /* Author: Mark Moll */
-#include <collision.hpp>
-#include <ompl/base/spaces/DubinsStateSpace.h>
-#include <ompl/base/spaces/ReedsSheppStateSpace.h>
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
 #include <ompl/base/ScopedState.h>
-#include <ompl/geometric/SimpleSetup.h>
-#include <ompl/geometric/planners/rrt/RRTstar.h>
 #include <ompl/base/objectives/PathLengthOptimizationObjective.h>
 #include <ompl/base/objectives/StateCostIntegralObjective.h>
+#include <ompl/base/spaces/DubinsStateSpace.h>
+#include <ompl/base/spaces/ReedsSheppStateSpace.h>
+#include <ompl/geometric/SimpleSetup.h>
+#include <ompl/geometric/planners/rrt/RRTstar.h>
+#include <pathserver.grpc.pb.h>
 #include <cmath>
+#include <collision.hpp>
 #include <fstream>
 #include <optional>
 
@@ -49,218 +54,175 @@ namespace ob = ompl::base;
 namespace og = ompl::geometric;
 
 // Objective that minimizes the number of states
-class StateMinimizationObjective: public ob::StateCostIntegralObjective {
-public:
-    // We don't need motion interpolation.
-    StateMinimizationObjective(const ob::SpaceInformationPtr& si) :
-        ob::StateCostIntegralObjective(si, true) {}
- 
-    ob::Cost stateCost(const ob::State *s) const override {
-        return ob::Cost{1.};
-    }
+class StateMinimizationObjective : public ob::StateCostIntegralObjective {
+ public:
+  // We don't need motion interpolation.
+  StateMinimizationObjective(const ob::SpaceInformationPtr& si)
+      : ob::StateCostIntegralObjective(si, true) {}
+
+  ob::Cost stateCost(const ob::State* s) const override { return ob::Cost{1.}; }
 };
 
-/**
- * Turning directions for the robot.
- */
-enum Direction {
-    STRAIGHT = 0,
-    LEFT,
-    RIGHT
-};
+using OptDirection = std::optional<pathserver::PlanReply_Move_Direction>;
+constexpr OptDirection map_direction(
+    ob::ReedsSheppStateSpace::ReedsSheppPathSegmentType t) {
+  using PMD = pathserver::PlanReply_Move_Direction;
 
-std::ostream &operator<<(std::ostream &os, const Direction d) {
-    static constexpr const char *map[] = {"STRAIGHT", "LEFT", "RIGHT"};
+  constexpr OptDirection map[] = {
+      OptDirection{}, OptDirection{PMD::PlanReply_Move_Direction_LEFT},
+      OptDirection{PMD::PlanReply_Move_Direction_STRAIGHT},
+      OptDirection{PMD::PlanReply_Move_Direction_RIGHT}};
 
-    return os << map[d];
+  return map[t];
 }
 
-using OptDirection = std::optional<Direction>;
-constexpr OptDirection map_direction(ob::ReedsSheppStateSpace::ReedsSheppPathSegmentType t) {
-
-    constexpr OptDirection map[] = {
-        OptDirection{}, OptDirection{Direction::LEFT}, 
-        OptDirection{Direction::STRAIGHT}, OptDirection{Direction::RIGHT}
-    };
-
-    return map[t];
-}
-
-/**
- * Moves for the robot.
- */
-struct Move {
-    /**
-     * Direction for the move.
-     */
-    Direction dir;
-    /**
-     * Move distance. In metres.
-     */
-    double distance;
-};
-
-std::ostream &operator<<(std::ostream &os, const Move &m) {
-    return os << m.dir << ' ' << m.distance;
-}
-
-
+using Move = pathserver::PlanReply_Move;
 /**
  * Generate moves for all states in the path.
- * 
+ *
  * \param[in] s state space
  * \param[in] p path that involves transitions between states in \c s.
  * \param[out] out destination where moves will be appended to. Not cleared,
  *  but the last state may be modified during path simplification.
- * 
- * \pre \c p.getStateCount() >= 2
+ *
+ * \pre \c p.getStateCount() >= 2 (i.e. when path is valid).
  */
-void generate_moves(const ob::ReedsSheppStateSpace &s, 
-                    const og::PathGeometric &p,
+void generate_moves(const ob::ReedsSheppStateSpace& s,
+                    const og::PathGeometric& p,
                     double turn_radius,
-                    std::vector<Move> &out) {
-    
-    auto initial_state = p.getState(0);
-    for (std::size_t i{1}; i < p.getStateCount(); ++i) {
-        auto target_state = p.getState(i);
-        auto path = s.reedsShepp(initial_state, target_state);
+                    std::vector<Move>& out) {
+  auto initial_state = p.getState(0);
+  for (std::size_t i{1}; i < p.getStateCount(); ++i) {
+    auto target_state = p.getState(i);
+    auto path = s.reedsShepp(initial_state, target_state);
 
-        for (std::size_t ps{}; ps < 5; ++ps) {
-            auto dir = map_direction(path.type_[ps]);
-            if (!dir.has_value())
-                continue;
+    for (std::size_t ps{}; ps < 5; ++ps) {
+      auto dir = map_direction(path.type_[ps]);
+      if (!dir.has_value())
+        continue;
 
-            double length_m = path.length_[ps] * turn_radius;
-            // Skip _actual_ zero length paths.
-            if (std::fpclassify(length_m) == FP_ZERO)
-                continue;
+      double length_m = path.length_[ps] * turn_radius;
+      // Skip _actual_ zero length paths.
+      if (std::fpclassify(length_m) == FP_ZERO)
+        continue;
 
-            if (out.size() && (out.back().dir == *dir)) {
-                // Attach move onto the previous move.
-                out.back().distance += length_m;
-            } else {
-                // Make a new move.
-                out.emplace_back(*dir, length_m);
-            }
-        }
-
-        initial_state = target_state;
-    }
-}
-
-/**
- * Print moves to an output stream.
- * 
- * \param mv moves.
- * \param to output stream to write to.
- */
-std::ostream &print_moves(const std::vector<Move> &mv, std::ostream &to) {
-    for (auto &move : mv) {
-        to << move << '\n';
+      if (out.size() && (out.back().direction() == *dir)) {
+        // Attach move onto the previous move.
+        out.back().set_distance(out.back().distance() + length_m);
+      } else {
+        // Add a new move.
+        Move added{};
+        added.set_distance(length_m);
+        added.set_direction(*dir);
+        out.emplace_back(added);
+      }
     }
 
-    return to;
+    initial_state = target_state;
+  }
 }
 
-void plan(const ob::StateSpacePtr &space)
-{
-    ob::ScopedState<> start(space), goal(space);
-    
-    // Setup bounding box.
-    ob::RealVectorBounds bounds(2);
-    bounds.setLow(0);
-    bounds.setHigh(WORLD_LENGTH);
-
-    space->as<ob::SE2StateSpace>()->setBounds(bounds);
-
+class PathServerImpl final : public pathserver::PathServer::Service {
+ public:
+  grpc::Status Plan(grpc::ServerContext* context,
+                    const pathserver::PlanRequest* request,
+                    pathserver::PlanReply* response) override {
     // Setup collision detector.
     CollisionDetector cdet{0.15, 0.10, 0.10, 0.10};
-    // Add obstacles to collision detector.
-    cdet.add_obstacle(0.25, 0.65); // (2.6)
-    cdet.add_obstacle(0.55, 0.75); // (5.7)
-    cdet.add_obstacle(0.95, 1.05); // (9.10)
+    for (const auto& ob : request->obstacles()) {
+      cdet.add_obstacle(ob.x() + 0.05, ob.y() + 0.05);
+    }
 
-    // define a simple setup class
-    og::SimpleSetup ss(space);
+    // Setup OMPL
+    ob::StateSpacePtr space = std::make_shared<ob::ReedsSheppStateSpace>(0.337);
+    ob::ScopedState<> start{space}, goal{space};
 
-    // set state validity checking for this space
-    const ob::SpaceInformation *si = ss.getSpaceInformation().get();
-    ss.setStateValidityChecker([cdet, si](const ob::State *state)
-                               { 
-                                   const auto *s = state->as<ob::SE2StateSpace::StateType>();
-                                   return (!cdet.check_collision(s->getX(), s->getY(), s->getYaw())) && (si->satisfiesBounds(state));
-                               });
+    // Setup bounding box.
+    ob::RealVectorBounds bounds{2};
+    bounds.setLow(0);
+    bounds.setHigh(WORLD_LENGTH);
+    space->as<ob::SE2StateSpace>()->setBounds(bounds);
 
-    //Configure RRT* planner.
-    ob::PlannerPtr planner = std::make_shared<ompl::geometric::RRTstar>(ss.getSpaceInformation());
+    og::SimpleSetup ss{space};
 
-    // Minimize path length while minimizing number of states.
-    ob::OptimizationObjectivePtr obj = 10 * std::make_shared<StateMinimizationObjective>(ss.getSpaceInformation()) + 
-        std::make_shared<ob::PathLengthOptimizationObjective>(ss.getSpaceInformation());
-    // ss.setOptimizationObjective(obj);
+    // Set state validity checking.
+    const ob::SpaceInformation* si = ss.getSpaceInformation().get();
+    ss.setStateValidityChecker([cdet, si](const ob::State* state) {
+      const auto* s = state->as<ob::SE2StateSpace::StateType>();
+      // Validate for collisions and whether the robot is within its boundaries.
+      return (!cdet.check_collision(s->getX(), s->getY(), s->getYaw())) &&
+             (si->satisfiesBounds(state));
+    });
+
+    // Configure RRT* planner.
+    ob::PlannerPtr planner =
+        std::make_shared<ompl::geometric::RRTstar>(ss.getSpaceInformation());
     ss.setPlanner(planner);
 
+    // TODO: only use if the default paths aren't great.
+    // Minimize path length while minimizing number of states.
+    // ob::OptimizationObjectivePtr obj = 10 *
+    // std::make_shared<StateMinimizationObjective>(ss.getSpaceInformation()) +
+    //                                 std::make_shared<ob::PathLengthOptimizationObjective>(ss.getSpaceInformation());
+    // ss.setOptimizationObjective(obj);
+
     // Set start and goal states.
-    // (.15, .15) 90 deg
-    start[0] = start[1] = .15;
-    start[2] = .5 * M_PI;
-    
-    // In front of obstacle (5.10)
-    goal[0] = .55;
-    goal[1] = 1.0;
-    goal[2] = 0 * M_PI;
+    start[0] = request->current().x();
+    start[1] = request->current().y();
+    start[2] = request->current().theta();
+    goal[0] = request->target().x();
+    goal[1] = request->target().y();
+    goal[2] = request->target().theta();
     ss.setStartAndGoalStates(start, goal);
 
-    // this call is optional, but we put it in to get more output information
+    // State check at .5% resolution.
     ss.getSpaceInformation()->setStateValidityCheckingResolution(0.005);
     ss.setup();
-    ss.print();
 
-    // attempt to solve the problem within 2 seconds of planning time
+    // Attempt to solve within 2s.
     ob::PlannerStatus solved = ss.solve(2);
 
-    if (solved)
-    {
-        std::vector<double> reals;
+    // If an approximate or exact solution is achieved.
+    if (solved) {
+      // Use one second to simplify the solution.
+      ss.simplifySolution(1.0);
+      og::PathGeometric path = ss.getSolutionPath();
 
-        std::cout << "Found solution:" << std::endl;
-        // Use one second to simplify the solution.
-        ss.simplifySolution(1.0);
-        og::PathGeometric path = ss.getSolutionPath();
+      // Not very efficient as we are doing multiple allocations.
+      // But it's nice for debugging.
+      std::vector<Move> moves{};
+      generate_moves(*(space->as<ob::ReedsSheppStateSpace>()), path, .337,
+                     moves);
+      for (auto& move : moves) {
+        auto mp = response->add_moves();
+        mp->CopyFrom(move);
+      }
 
-        std::vector<Move> moves{};
-        generate_moves(*(space->as<ob::ReedsSheppStateSpace>()), path, .337, moves);
-        
-        std::ofstream out_moves{"moves.csv"};
-        print_moves(moves, out_moves);
+      for (std::size_t i{}; i < path.getStateCount(); ++i) {
+        const auto* s = path.getState(i)->as<ob::SE2StateSpace::StateType>();
+        auto sp = response->add_waypoints();
+        sp->set_x(s->getX());
+        sp->set_y(s->getY());
+        sp->set_theta(s->getYaw());
+      }
 
-        std::ofstream out_states{"states.csv"};
-        path.printAsMatrix(out_states);
+      return grpc::Status::OK;
+    } else
+      return grpc::Status{grpc::StatusCode::DEADLINE_EXCEEDED,
+                          "no solution found within planning time limit"};
+  }
+};
 
-        std::ofstream out_interp{"path.csv"};
-        path.interpolate(1000);
-        path.printAsMatrix(out_interp);
-    }
-    else
-        std::cout << "No solution found" << std::endl;
-}
+int main(int, char**) {
+  std::string addr{"0.0.0.0:10003"};
+  PathServerImpl service{};
 
-int main(int argc, char *argv[])
-{
-    try
-    {
-        auto space = std::make_shared<ob::ReedsSheppStateSpace>(0.337);
-        plan(space);
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << "error: " << e.what() << "\n";
-        return 1;
-    }
-    catch (...)
-    {
-        std::cerr << "Exception of unknown type!\n";
-    }
+  grpc::ServerBuilder builder{};
+  builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
 
-    return 0;
+  auto server = builder.BuildAndStart();
+  server->Wait();
+
+  return 0;
 }
