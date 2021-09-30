ca353030e697d76b92031c158791926224145e2d
Shenghao Yang <me@shenghaoyang.info>
2021-09-30 16:25:13 +0800

    collision: re-org with ABC

--- collision.cpp
+++ collision.cpp
@@ -3,8 +3,6 @@
 #include <collision.hpp>
 #include <vector>
 
-constexpr std::array<c2Ray, 4> CollisionDetector::edges;
-
 CollisionDetector::CollisionDetector(float f2c,
                                      float b2c,
                                      float l2c,
@@ -18,7 +16,15 @@ CollisionDetector::CollisionDetector(float f2c,
   c2MakePoly(&robot_shape);
 }
 
-void CollisionDetector::add_obstacle(float x, float y) {
+constexpr std::array<c2Ray, 4> CollisionDetectorIR::edges;
+
+CollisionDetectorIR::CollisionDetectorIR(float f2c,
+                                         float b2c,
+                                         float l2c,
+                                         float r2c)
+    : CollisionDetector{f2c, b2c, l2c, r2c} {}
+
+void CollisionDetectorIR::add_obstacle(float x, float y) {
   c2v obs_ctr{x, y};
   c2v obs_offset{OBSTACLE_LENGTH / 2, OBSTACLE_LENGTH / 2};
   c2AABB obs{c2Sub(obs_ctr, obs_offset), c2Add(obs_ctr, obs_offset)};
@@ -26,11 +32,11 @@ void CollisionDetector::add_obstacle(float x, float y) {
   obstacles.push_back(obs);
 }
 
-void CollisionDetector::clear_obstacles() {
+void CollisionDetectorIR::clear_obstacles() {
   obstacles.clear();
 }
 
-bool CollisionDetector::check_collision(float x, float y, float theta) const {
+bool CollisionDetectorIR::check_collision(float x, float y, float theta) const {
   // Transform for robot shape.
   c2x xfrm{c2v{x, y}, c2Rot(theta)};
 
@@ -46,6 +52,51 @@ bool CollisionDetector::check_collision(float x, float y, float theta) const {
     if (c2AABBtoPoly(o, &robot_shape, &xfrm))
       return true;
   }
+
+  return false;
+}
+
+constexpr std::array<c2AABB, 3> CollisionDetectorFC::carpark;
+
+CollisionDetectorFC::CollisionDetectorFC(float f2c,
+                                         float b2c,
+                                         float l2c,
+                                         float r2c,
+                                         float d)
+    : CollisionDetector{f2c, b2c, l2c, r2c},
+      obstacle{c2v{0.5f + d, -0.30}, c2v{0.6f + d, 0.30}} {
+  // Robot initially centered around (0, 0), with rotation = 0.
+  robot_shape.count = 4;
+  robot_shape.verts[0] = c2v{f2c, l2c};
+  robot_shape.verts[1] = c2v{f2c, -r2c};
+  robot_shape.verts[2] = c2v{-b2c, -r2c};
+  robot_shape.verts[3] = c2v{-b2c, l2c};
+  c2MakePoly(&robot_shape);
+}
+
+/**
+ * Check if the robot has collided with any object in the play area.
+ *
+ * \param x x-coordinate of robot's reference point.
+ * \param y y-coordinate of robot's reference point.
+ * \param theta robot's rotation in radians.
+ *
+ * \retval true if a collision is detected.
+ * \retval false if a collision is not detected.
+ */
+bool CollisionDetectorFC::check_collision(float x, float y, float theta) const {
+  // Transform for robot shape.
+  c2x xfrm{c2v{x, y}, c2Rot(theta)};
+
+  // Check collisions against carpark obstacles.
+  for (const auto o : carpark) {
+    if (c2AABBtoPoly(o, &robot_shape, &xfrm))
+      return true;
+  }
+
+  // Check collision against main obstacle.
+  if (c2AABBtoPoly(obstacle, &robot_shape, &xfrm))
+    return true;
 
   return false;
 }
--- collision.hpp
+++ collision.hpp
@@ -12,8 +12,40 @@ constexpr float WORLD_LENGTH{2.0};
 constexpr float OBSTACLE_LENGTH{0.1};
 
 /**
- * Collision detector class that can be used to detect whether the robot has
- * collided with any elements in the MDP world.
+ * Base class for all collision detectors.
+ */
+class CollisionDetector {
+ protected:
+  /// Polygonal shape representing the robot.
+  c2Poly robot_shape;
+
+ public:
+  /**
+   * Create a new collision detector.
+   *
+   * \param f2c distance from front of robot to centre.
+   * \param b2c distance from back of robot to centre.
+   * \param l2c distance from left of robot to centre.
+   * \param r2c distance from right of robot to centre.
+   */
+  CollisionDetector(float f2c, float b2c, float l2c, float r2c);
+  /**
+   * Check if the robot has collided with any object in the play area.
+   *
+   * \param x x-coordinate of robot's reference point.
+   * \param y y-coordinate of robot's reference point.
+   * \param theta robot's rotation in radians.
+   *
+   * \retval true if a collision is detected.
+   * \retval false if a collision is not detected.
+   */
+  virtual bool check_collision(float x, float y, float theta) const = 0;
+
+  virtual ~CollisionDetector() = default;
+};
+
+/**
+ * Collision detector for the image recognition task.
  *
  * Both obstacles and the world's boundaries are defined as objects that the
  * robot can collide with.
@@ -21,10 +53,10 @@ constexpr float OBSTACLE_LENGTH{0.1};
  * The MDP "world" is defined to exist from \c (0, WORLD_LENGTH) to
  * \c (WORLD_LENGTH, WORLD_HEIGHT).
  *
- * Where \c (0, WORLD_LENGTH) is the bottom left corner of the play area, and
- * \c (WORLD_LENGTH, WORLD_HEIGHT) is the top right corner of the play area.
+ * Where \c (0, \c WORLD_LENGTH) is the bottom left corner of the play area, and
+ * \c (WORLD_LENGTH, \c WORLD_HEIGHT) is the top right corner of the play area.
  */
-class CollisionDetector {
+class CollisionDetectorIR : public CollisionDetector {
  private:
   /// Rays representing the edges of the play area.
   static constexpr std::array<c2Ray, 4> edges = {
@@ -36,8 +68,6 @@ class CollisionDetector {
       c2Ray{c2v{0., 0.}, c2v{0., 1.}, WORLD_LENGTH},
       // Right edge
       c2Ray{c2v{WORLD_LENGTH, 0.}, c2v{0., 1.}, WORLD_LENGTH}};
-  /// Polygonal shape representing the robot.
-  c2Poly robot_shape;
   /// AABBs representing the obstacles.
   std::vector<c2AABB> obstacles;
 
@@ -53,7 +83,7 @@ class CollisionDetector {
    * \param l2c distance from left of robot to centre.
    * \param r2c distance from right of robot to centre.
    */
-  CollisionDetector(float f2c, float b2c, float l2c, float r2c);
+  CollisionDetectorIR(float f2c, float b2c, float l2c, float r2c);
 
   /**
    * Add an obstacle to the collision detector.
@@ -68,15 +98,41 @@ class CollisionDetector {
    */
   void clear_obstacles();
 
+  bool check_collision(float x, float y, float theta) const override;
+};
+
+/**
+ * Collision detector class that can be used to detect whether the robot has
+ * collided with any elements in the MDP world.
+ *
+ * Collision detector for the fastest car task.
+ */
+class CollisionDetectorFC : public CollisionDetector {
+ private:
+  /// AABBs representing the obstacles used to create the carpark.
+  static constexpr std::array<c2AABB, 3> carpark = {
+      {{c2v{-0.1, -0.3}, c2v{0, 0.3}},
+       {c2v{-0.1, -0.4}, c2v{0.5, -0.3}},
+       {c2v{-0.1, 0.3}, c2v{0.5, 0.4}}}};
+  /// Polygonal shape representing the robot.
+  c2Poly robot_shape;
+  /// AABB representing the obstacle.
+  const c2AABB obstacle;
+
+ public:
+  // Copy construction and copy-assign are all okay.
+  // Move construction and move-assign are all okay.
+
   /**
-   * Check if the robot has collided with any object in the play area.
+   * Create a new collision detector.
    *
-   * \param x x-coordinate of robot's reference point.
-   * \param y y-coordinate of robot's reference point.
-   * \param theta robot's rotation in radians.
-   *
-   * \retval true if a collision is detected.
-   * \retval false if a collision is not detected.
+   * \param f2c distance from front of robot to centre.
+   * \param b2c distance from back of robot to centre.
+   * \param l2c distance from left of robot to centre.
+   * \param r2c distance from right of robot to centre.
+   * \param d distance from center of obstacle to edge of carpark.
    */
-  bool check_collision(float x, float y, float theta) const;
+  CollisionDetectorFC(float f2c, float b2c, float l2c, float r2c, float d);
+
+  bool check_collision(float x, float y, float theta) const override;
 };
